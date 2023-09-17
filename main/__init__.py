9697ec588df370b1879653a2c0ce6acde7fa3bbb
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2023-09-17 13:04:44 +0530

    Update __init__.py

--- main/__init__.py
+++ main/__init__.py
@@ -16,7 +16,7 @@ API_ID = 3704772
 API_HASH = "b8e50a035abb851c0dd424e14cac4c06"
 BOT_TOKEN = "1064540465:AAFTgEC_xtVSiu3RdoESorVfy_cAwZ5K0Qw"
 SESSION = "BQA4h8QAwnajp2P3Ycqu1ZXe-q9n0-Sz0ZY_y8Of0YdawntmdjkIUePKbT93geeWo6t3n4CxK7l7RenDhh_QxmBUh3v6Iv7Yt9EReGlbXDbKElyhxx9KlFsH6jIsQzHd2SJcqFxGpxJr5o96XLCVL4DJ3H7XnpjEQBkoG5_1N6c-N01vU5eC8XjNVZQdLobg9h-PcvpnJCxBpUHQrUbdMu_kgJMgHjZfh4diOKkEM5vBs7MaYAiVv-zLInetIdT57rKs2ZHsLYlxqx76wGQAdDwhmFzA1tpHleR2GaQ5Jimcibb-uZphWbkdU2OIEQI3dskiH44Z0Nm4301tri0mQamLdsIGagAAAAAd2TbUAA"
-FORCESUB = rootedcyberchannel2
+FORCESUB = "rootedcyberchannel2"
 AUTH = 500774612
 
 bot = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN) 
